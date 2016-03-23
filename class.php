2f46a962a59e249ae80c6a283aaeca13aa09ab84
Stoyan Kyosev <stoyankyosev@gmail.com>
2016-03-23 10:27:08 +0200

    merge conflicts

--- class.php
+++ class.php
@@ -2,8 +2,14 @@
 
 class Utils 
 {
+<<<<<<< 580c6ea9db21e0f441b3633a9d2842fd728a2da9
     public function getName()
     {
         return 'Utils';  
+=======
+    public function __construct() 
+    {
+        
+>>>>>>> 2d05ae09a2d5970e92e92607a4e8f6d8712dc3af
     }
 }
